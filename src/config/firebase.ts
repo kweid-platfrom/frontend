96dd66c7b7ccfcf6bca291a14a41aece27b9f9ff
Fidelis <Fidelis>
2025-03-06 14:40:01 +0100

    Settings

---
+++ src/config/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6YHy9gOuouEwy8JufyfAcXIUicTXU7ZY",
+    authDomain: "qaid-3f040.firebaseapp.com",
+    projectId: "qaid-3f040",
+    storageBucket: "qaid-3f040.firebasestorage.app",
+    messagingSenderId: "347962756282",
+    appId: "1:347962756282:web:3ac695f30ec9a552d6c683",
+    measurementId: "G-1DY8Y53ZCP"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
