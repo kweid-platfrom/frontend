ca83a594da970a74383d53156571eb990fca5904
Fidelis <Fidelis>
2025-03-07 21:09:41 +0100

    more auth setup

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-import { signInWithPopup, signOut } from "firebase/auth";
+import { sendEmailVerification, createUserWithEmailAndPassword, signInWithPopup, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth, googleProvider } from "../config/firebase";
 
 const signInWithGoogle = async () => {
@@ -13,6 +13,25 @@ const logout = async () => {
     await signOut(auth);
 }
 
+const registerWithEmail = async (email: string, password: string) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        await sendEmailVerification(userCredential.user);
+        console.log("Verification email sent!");
+    } catch (error) {
+        console.error("Registration error:", error);
+    }
+};
+
+const LogInWithEmail = async (email: string, password: string) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        console.log("User Logged in successfully!"); 
+    } catch (error) {
+        console.error("Login error:", error);
+    }
+};
+
 export {
-    signInWithGoogle, logout
+    signInWithGoogle, LogInWithEmail, logout, registerWithEmail
 };
