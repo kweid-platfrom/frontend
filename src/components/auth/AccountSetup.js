1c11b56f3641a219bc6089327380db8fe9958dea
Fidelis <Fidelis>
2025-03-08 00:02:32 +0100

    added Register code and account set up

---
+++ src/components/auth/AccountSetup.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { isSignInWithEmailLink, signInWithEmailLink, updatePassword } from "firebase/auth";
+import { auth } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
+
+const AccountSetup = () => {
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [name, setName] = useState("");
+    const [company, setCompany] = useState("");
+
+    const navigaten = useNavigate();
+    const email = window.localStorage.getItem("emailForSignIn")
+
+    useEffect(() => {
+        if (isSignInWithEmailLink(auth, window.location.href)) {
+            signInWithEmailLink(auth, email, window.location.href).then(() => {
+                window.localStorage.removeItem("emailForSignIn");
+            })
+                .catch((error) => console.error(error.message));
+        }
+    }, [email]);
+
+    const handleSetpassword = async () => {
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        try {
+            await updatePassword(auth.currentUser, password);
+            await setDoc(doc(db, "users", auth.currentUser.uid), {
+                name, company, email: auth.currentUser.eamil,
+            });
+            alert("Account setup complete.");
+            navigaten("/add-users")
+        } catch (error) {
+            console.error(error.message)
+        }
+    };
+
+    return (
+        <div>
+            <h2>Set Up Your Acoount</h2>
+            <input type="text" placeholder="Full name" onChange={(e) => setName(e.target.value)} required />
+            <input type="text" placeholder="Company name" onChange={(e) => setCompany(e.target.value)} required />
+            <input type="password" placeholder="New Password" onChange={(e) => setPassword(e.target.value)} required />
+            <input type="password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} required />
+            <button onClick={handleSetpassword}>Continue</button>
+        </div>
+    )
+
+
+}
+
+export default AccountSetup;
---
+++ src/components/auth/Register.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { sendSignInLinkToEmail, signInWithPopup, } from "firebase/auth";
+import { auth, googleProvider } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+            const actionCodeSettings = {
+                url: "http://localhost:3000/set-password", handleCodeInApp: true,
+            };
+            await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+            window.localStorage.setItem("emailForSignIn", email)
+            alert("Verification email sent. Please check your inbox.");
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const handleGoogleRegister = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+            navigate("/dashboard");
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    return (
+        <div>
+
+            <h2>Register</h2>
+            <button onClick={handleGoogleRegister}>Google</button>
+            <span>or Register with </span>
+            <form action="form" onSubmit={handleRegister}>
+                <input type="email" placeholder="Company email" on onChange={(e) => setEmail(e.target.value)} required />
+                <button type="submit">Register</button>
+            </form>
+            
+        </div>
+    );
+
+
+};
+
+export default Register;
