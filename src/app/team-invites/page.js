c120f88c12373765e4f8b742a8a90ad771229262
Fidelis <Fidelis>
2025-08-06 16:47:31 +0100

    fix

--- src/app/team-invites/page.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import TeamInviteForm from "../../components/onboarding/TeamInviteForm";
-
-export default function TeamInviteFormPage() {
-    return <TeamInviteForm />
-}
