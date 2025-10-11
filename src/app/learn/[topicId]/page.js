de6f9c1c9f6cbf73b5c0ad9e2efa02c7abd2c1d5
Fidelis Agba <phyd3lis@yandex.com>
2025-10-11 17:36:50 +0100

    eslint error fix

--- src/app/learn/[topicId]/page.js
+++ src/app/learn/[topicId]/page.js
@@ -1,12 +1,1 @@
-// src/app/learn/[topicId]/page.jsx
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const TopicDetailPage = dynamic(() => import('@learning-module/app/learn/[topicId]/page'), {
-  ssr: false,
-});
-
-export default function Topic() {
-  return <TopicDetailPage />;
-}
+export { default } from '@learning-module/app/learn/[topicId]/page';
--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -1,12 +1,1 @@
-// src/app/learn/page.jsx
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const LearningApp = dynamic(() => import('@learning-module/app/learn/page'), {
-  ssr: false,
-});
-
-export default function LearnPage() {
-  return <LearningApp />;
-}
+export { default } from '@learning-module/app/learn/page';
